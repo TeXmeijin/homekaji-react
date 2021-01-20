61193066911174d4bc4876d8031e24998ce2658e
TeXmeijin <i8858garden@gmail.com>
2021-01-21 01:30:30 +0900

    initial commit

---
+++ components/entities/HouseworkListItem.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {housework: {name: string, point: number}}
+
+export const HouseworkListItem = ({housework}: Props) => {
+  return (
+    <li
+      className="flex items-center justify-between py-6 px-6 shadow rounded-2xl"
+    >
+  <span className="text-xl font-bold">
+        {housework.name}
+      </span>
+      <span className="text-3xl text-gray-700"> {housework.point}pt </span>
+    </li>
+  )
+}
